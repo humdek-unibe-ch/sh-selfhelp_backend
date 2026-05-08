263abd64f9547b58417917c427b3ce479d94af75
Rui Henriques <rui.daconceicao@unibe.ch>
2026-05-08 14:13:35 +0200

    migration to remove duplicate section in two factor auth system page

---
+++ migrations/Version20260508121229.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260508121229 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Delete section with id 69';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('DELETE FROM selfhelp2.sections WHERE id = 69');
+    }
+    public function down(Schema $schema): void {}
+}
