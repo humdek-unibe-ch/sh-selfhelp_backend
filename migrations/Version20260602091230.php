a0675735df6b1bf3df97fbb9004299dbf284e5f6
Rui Henriques <rui.daconceicao@unibe.ch>
2026-06-02 11:27:25 +0200

    remove down from delete related

--- migrations/Version20260602091230.php
+++ migrations/Version20260602091230.php
@@ -49,31 +49,6 @@ final class Version20260602091230 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->addSql(<<<SQL
-            INSERT IGNORE INTO `permissions` (`name`, `description`)
-            VALUES ('admin.registration_code.delete', 'Can delete registration codes')
-        SQL);
-
-        $this->addSql(<<<SQL
-            INSERT IGNORE INTO `api_routes`
-                (`route_name`, `version`, `methods`, `path`, `controller`, `requirements`, `params`)
-            VALUES
-                ('admin_registration_codes_delete', 'v1', 'DELETE', '/admin/registration-codes/{code}', 'App\\\\Controller\\\\Api\\\\V1\\\\Admin\\\\AdminRegistrationCodeController::delete', NULL, NULL)
-        SQL);
-
-        $this->addSql(<<<SQL
-            INSERT IGNORE INTO `rel_permissions_roles` (`id_permissions`, `id_roles`)
-            SELECT p.id, r.id
-            FROM `permissions` p, `roles` r
-            WHERE p.`name` = 'admin.registration_code.delete' AND r.`name` = 'admin'
-        SQL);
-
-        $this->addSql(<<<SQL
-            INSERT IGNORE INTO `rel_api_routes_permissions` (`id_api_routes`, `id_permissions`)
-            SELECT ar.id, p.id
-            FROM `api_routes` ar
-            JOIN `permissions` p ON p.`name` = 'admin.registration_code.delete'
-            WHERE ar.`route_name` = 'admin_registration_codes_delete' AND ar.`version` = 'v1'
-        SQL);
+        $this->throwIrreversibleMigrationException();
     }
 }
